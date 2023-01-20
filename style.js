1cf759f34802a86973c8a5a1a4b8960145b5bcf2
cjgabilan1 <74236635+cjgabilan1@users.noreply.github.com>
2023-01-20 18:22:20 +0800

    add image and spelling correction

--- style.js
+++ style.js
@@ -50,3 +50,22 @@ var x = setInterval(function() {
       }, false)
     })
 })()
+
+
+   
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  responsive:{
+      0:{
+          items:2
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:4
+      }
+  }
+})
